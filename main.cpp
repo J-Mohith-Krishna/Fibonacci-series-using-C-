46248a5d266ee4a5ea86dacddd55c2e97877ffe8
Mohith Krishna J <126872367+J-Mohith-Krishna@users.noreply.github.com>
2024-03-03 22:03:49 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n) {
+    int first = 0, second = 1, next;
+
+    cout << "Fibonacci Series up to " << n << " terms:" << endl;
+
+    for (int i = 0; i < n; ++i) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout << next << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of terms in Fibonacci series: ";
+    cin >> n;
+
+    fibonacci(n);
+
+    return 0;
+}
